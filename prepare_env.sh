4a9f0bd5fb174ebe479efb9d7102ffd65ba67c38
watislaf <vladkozulin@mail.ru>
2022-04-20 23:51:25 +0300

    Dostalem warning na linuxie, wiec poprawilem script

--- prepare_env.sh
+++ prepare_env.sh
@@ -8,4 +8,5 @@ for pre_hook_file_path in ${PROJECT_HOOKS}; do
   NAME_OF_HOOK=$(basename ${pre_hook_file_path})
   echo "Add ${NAME_OF_HOOK} hook."
   cat ${pre_hook_file_path} >"${GIT_HOOKS}${NAME_OF_HOOK}"
+  chmod +x ${GIT_HOOKS}${NAME_OF_HOOK}
 done
