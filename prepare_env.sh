5bcb81bc6b45fa01aef8b2a22d284a09d9fb2382
watislaf <vladkozulin@mail.ru>
2022-04-20 19:51:42 +0200

    Dodalem commit hook

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
 
 SCRIPT_DIR=$(dirname "$0")
-GIT_PRE_PUSH=${SCRIPT_DIR}"/.git/hooks/pre-push"
-echo ${SCRIPT_DIR}
-if [ ! -f "${GIT_PRE_PUSH}" ]; then
-  echo "Create git-hub hooks"
-  cat "${SCRIPT_DIR}""/hooks/pre-push" >${GIT_PRE_PUSH}
-fi
+GIT_HOOKS=${SCRIPT_DIR}"/.git/hooks/"
+PROJECT_HOOKS="${SCRIPT_DIR}/hooks/*"
+
+for pre_hook_file_path in ${PROJECT_HOOKS}; do
+  NAME_OF_HOOK=$(basename ${pre_hook_file_path})
+  echo "Add ${NAME_OF_HOOK} hook."
+  cat ${pre_hook_file_path} >"${GIT_HOOKS}${NAME_OF_HOOK}"
+done
