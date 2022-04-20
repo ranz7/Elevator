4ec540a7905e848f034513caaa6abfa54c17c7e9
watislaf <vladkozulin@mail.ru>
2022-04-20 17:57:26 +0200

    Małe zmiany

--- window/src/main/java/controller/Main.java
+++ window/src/main/java/controller/Main.java
@@ -4,5 +4,11 @@ package controller;
 public class Main {
     public static void main(String[] args) {
         System.out.println("HELLO I AM VIEW");
+
+        if (args.length == 0) {
+            System.out.println("local host");
+        } else {
+            System.out.println(args[1]);
+        }
     }
 }
