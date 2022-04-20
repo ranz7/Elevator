25dbc04ca375be2e5a5653078ef59138273000ec
Vlad Kozulin <45079123+watislaf@users.noreply.github.com>
2022-04-20 23:29:47 +0300

    Git hooks added (#3)
    * Git hooks added
    
    * Dodalem commit hook
    
    * Maly bug
    
    * Update pre-commit

---
+++ prepare_env.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+SCRIPT_DIR=$(dirname "$0")
+GIT_HOOKS=${SCRIPT_DIR}"/.git/hooks/"
+PROJECT_HOOKS="${SCRIPT_DIR}/hooks/*"
+
+for pre_hook_file_path in ${PROJECT_HOOKS}; do
+  NAME_OF_HOOK=$(basename ${pre_hook_file_path})
+  echo "Add ${NAME_OF_HOOK} hook."
+  cat ${pre_hook_file_path} >"${GIT_HOOKS}${NAME_OF_HOOK}"
+done
