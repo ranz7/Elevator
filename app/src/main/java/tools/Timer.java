a4cc4eece4a03e4e2db6d1f2210e40b86adf4761
PiJayson <pijay888@gmail.com>
2022-04-22 15:23:42 +0200

    fixes

--- app/src/main/java/tools/Timer.java
+++ app/src/main/java/tools/Timer.java
@@ -6,27 +6,25 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Timer {
     @Getter
-    private long currentTimer;
-    private long counterTime;
+    private long currentTime;
+    private long startTime;
 
     public void tick(long deltaTime) {
-        currentTimer -= deltaTime;
+        currentTime -= deltaTime;
     }
 
-
     public boolean isReady() {
-        return currentTimer <= 0;
+        return currentTime <= 0;
     }
 
     public void restart(long timeToCount) {
-        currentTimer = timeToCount;
-        counterTime = timeToCount;
+        startTime = currentTime = timeToCount;
     }
 
     public double getPercent() {
-        if (currentTimer <= 0) {
+        if (currentTime <= 0) {
             return 0;
         }
-        return currentTimer * 1. / counterTime;
+        return currentTime * 1. / startTime;
     }
 }
