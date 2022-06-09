67be485913005c18625c9f12668b3ea0376330cb
watislaf <vladkozulin@mail.ru>
2022-06-09 11:42:23 +0200

    X

--- app/src/main/java/settings/configs/AppControllerConfig.java
+++ app/src/main/java/settings/configs/AppControllerConfig.java
@@ -1,5 +1,5 @@
 package settings.configs;
 
 public class AppControllerConfig {
-    public static final int TPS = 120;
+    public static final int TPS = 200;
 }
--- connector/src/main/java/configs/ConnectionSettings.java
+++ connector/src/main/java/configs/ConnectionSettings.java
@@ -1,7 +1,7 @@
 package configs;
 
 public class ConnectionSettings {
-    static public final double SSPS = 24;       // server send per second
+    static public final double SSPS = 30;       // server send per second
     static public final String HOST = "localhost";
     public static final String HOST2 = "13.69.185.6";
     static public final int PORT = 6778;
